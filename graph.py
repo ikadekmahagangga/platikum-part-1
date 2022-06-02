fa06528d88e81319c00232a536f20b676ee775be
I KADEK MAHA GANGGA <102144626+ikadekmahagangga@users.noreply.github.com>
2022-06-02 08:42:06 +0000

    commit

---
+++ graph.py
@@ -0,0 +1,18 @@
+vertices = {"A", "B", "C", "D", "E", "F"}
+edges = {("A", "D"), ("A", "B"), ("A", "E"), ("A", "F"), ("B", "F"), ("B", "C")}
+graph = dict()
+for vertex in vertices:
+    graph[vertex] = []
+for edge in edges:
+    v1 = edge[0]
+    v2 = edge[1]
+    graph[v1].append(v2)
+    graph[v2].append(v1)
+print("The given set of vertices is:", vertices)
+print("The given set of edges is:", edges)
+print("Graph representation in python is:")
+print(graph)
+
+
+# referensi https://www.pythonforbeginners.com/data-structures/graph-in-python
+# ini adalah codingan basic dari graph sederhana

3ff07260da29ef358dd45b69ea3b996ca25f4d62
I KADEK MAHA GANGGA <102144626+ikadekmahagangga@users.noreply.github.com>
2022-06-02 11:25:55 +0000

    commit

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,20 +1,14 @@
 def bubbleSort(arr):
     n = len(arr)
- 
     for i in range(n):
-
         for j in range(0, n-i-1):
  
 
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
- 
- 
 
 arr = [4, 1, 5, 2, 3]
- 
 bubbleSort(arr)
- 
 print("Sorted array is:")
 for i in range(len(arr)):
     print("%d" % arr[i], end=" ")

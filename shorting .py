99717acc4f4cf6120442c3cfa71595ed3f10a2b2
I KADEK MAHA GANGGA <102144626+ikadekmahagangga@users.noreply.github.com>
2022-06-16 10:15:03 +0000

    up

--- shorting .py
+++ shorting .py
@@ -12,10 +12,9 @@ def selectionSort(array, size):
          
         # put min at the correct position
         (array[step], array[min_idx]) = (array[min_idx], array[step])
-
-print ("-2, 45, 0, 11, -9")
 print ("before shorted : ")
-data = [-2, 45, 0, 11, -9]
+print (-1, 2, 0, 1, -2)
+data = [-1, 2, 0, 1, -2]
 size = len(data)
 selectionSort(data, size)
 print("after it has been sorted : ")
